555a636c5672cfbd8b7e2c97ecc46341a9cd50f2
matlavv <mathis.laversin92@gmail.com>
2023-11-30 12:08:59 +0100

    HomePage style, get a ride & order food buttons

--- App.js
+++ App.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from "react-redux";
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { store } from './store';
+import HomeScreen from './screens/HomeScreen';
 
 //Setup redux
 
 export default function App() {
   return (
     <Provider store={store}>
-      <View style={styles.container}>
-        <Text>Uber</Text>
-      </View>
+      <SafeAreaProvider>
+        <HomeScreen />
+      </SafeAreaProvider>
     </Provider>
   );
 }
